38a9a48cee21785faca2c782af4e8fa1d28e1b95
diguage <leejun119@gmail.com>
2016-05-22 15:12:45 +0800

    finish copying the current page link to clipboard

---
+++ background.js
@@ -0,0 +1,6 @@
+// Follow: https://developer.chrome.com/extensions/examples/api/browserAction/make_page_red/background.js
+chrome.browserAction.onClicked.addListener(function(tab) {
+    var link = tab.url + '[' + tab.title + ']';
+    console.log(link);
+    copyToClipboard(link);
+})
